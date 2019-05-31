b9fdd82a9f61f806019150143981b2b54b1873eb
Luke Przekwas <luke@covalence.io>
2019-05-31 11:44:13 -0500

    adding comments

--- src/utils/useForm.js
+++ src/utils/useForm.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 
 const useForm = callback => {
-
+    
+    //initial state is an object
     const [values, setValues] = useState({});
 
+    //simply prevents form refresh and uses whatever callback we provide
     const handleSubmit = e => {
         if (e) e.preventDefault();
         callback();
@@ -11,9 +13,12 @@ const useForm = callback => {
 
     const handleChange = e => {
         e.persist();
+        //will update the values object and maintain state via spread ... operator!
+        //it will use the e.target.name to generate the key for the input
         setValues(values => ({ ...values, [e.target.name]: e.target.value }))
     }
 
+    //return our object for this hook
     return {
         handleChange,
         handleSubmit,
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -3,10 +3,15 @@ import useForm from '../utils/useForm';
 
 const Home = () => {
 
+    //our callback for handleSubmit
     const login = () => {
         console.log(values);
     }
 
+    //custom hook returns an object with these three properties
+    //values is an object with a key for each input we need
+    //handleChange is your univeral onChange handler
+    //handleSubmit is whatever you want as your callback, it just preventsDefault on form refresh
     const { values, handleChange, handleSubmit } = useForm(login);
 
     return (
@@ -17,6 +22,9 @@ const Home = () => {
                     className="form-group border border-dark rounded p-3 shadow-lg">
                     <label>Email Address</label>
                     <input
+                        //we need to do the || '' trick to make sure we have a controlled input
+                        //initial value of the values object is that .. an object and not a string
+                        //so react will throw warnings even if it works
                         value={values.email || ''}
                         onChange={handleChange}
                         className="form-control p-1 my-1 shadow-sm"
